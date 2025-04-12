2ae9649e52a8dee8e2d4e80fc339f6ebb1348f6e
saurav.choudhary <saurav.choudhary@cmssl.in>
2025-04-12 10:17:33 +0530

    chore: some changes

--- backend/app/api/routes/time_travel_controller.py
+++ backend/app/api/routes/time_travel_controller.py
@@ -1,5 +1,5 @@
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Header
 
 from app.config.logger import get_logger
 from app.service.time_travel_service import fetch_commit_history_with_diffs
@@ -11,12 +11,14 @@ logger = get_logger(__name__)
 
 @router.get("/time-travel")
 def get_all_data(
+    # X_Access_Token: str = Header(..., description="Access token from request header"),
+    # X_Github_Url: str = Header(..., description="Github URL from request header"),
+
 ):
     """
     Get all data from git repository.
     """
-    url = "https://github.com/369saurav/PlayGM/blob/master/core/usecase/playgm_usecase.py"
     logger.info("Fetching all data from git repository")
-    commits = fetch_commit_history_with_diffs(url)
+    commits = fetch_commit_history_with_diffs()
    
     return {"data": commits}
